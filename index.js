245e1b29fc9b4e88604971700e4fb272aed75a0d
Ruslan Kvachuk <rkvacuk@gmail.com>
2017-05-04 15:46:49 +0300

    commit

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//     elem.addEventListener("click" , function(){
+//        display = document.getElementById("hide").style.display;
+
+//     if(display=='none'){
+//        document.getElementById("hide").style.display='block';
+
+//     }else{
+
+//        document.getElementById("hide").style.display='none';
+
+//     }
+//  });
+
+    categoryTwo.addEventListener("click" , function(){
+        document.getElementById("hide").style.display='none';
+    });
+
+    
+    categoryOne.addEventListener("click" , function(){
+        document.getElementById("hide").style.display='block';
+    });
